0d417c6f447a25976906004bd4f2a0976f349e3e
Rodrigo <medeiros11.dev@gmail.com>
2023-01-15 20:24:30 -0300

    Final da aula 02

--- dart_curso2/bin/dart_curso2.dart
+++ dart_curso2/bin/dart_curso2.dart
@@ -6,10 +6,28 @@ void main() {
   int diasDesdeColheita = 40;
   bool isMadura = funcEstaMadura(diasDesdeColheita);
 
-  print(isMadura);
+  funcQuantosDiasMadura(diasDesdeColheita);
 }
 
-bool funcEstaMadura(int dias) {
+funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = diasParaMadura - dias;
+  print(quantosDiasFaltam);
+}
+
+void mostrarMadura(String nome, int dias, {String cor = "Sem cor"}) {
+  if (dias >= 30) {
+    print("A $nome está madura.");
+  } else {
+    print("A $nome não está madura.");
+  }
+
+  if (cor != null) {
+    print("A $nome é $cor");
+  }
+}
+
+funcEstaMadura(int dias) {
   if (dias >= 30) {
     return true;
   } else {
